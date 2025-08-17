de84f9b593f3db7df6a8ed3eee7a2f19731b50a7
mehulklh <106767917+mehulklh@users.noreply.github.com>
2025-08-17 12:35:48 +0530

    initial commit

---
+++ init.lua
@@ -0,0 +1,29 @@
+require("config.lazy")
+
+
+-- lsp setup
+
+vim.lsp.enable({
+	"clangd",
+	"gopls"
+})
+
+
+vim.diagnostic.config({
+	virtual_text = true,
+	virtual_lines = true,
+	underline= true,
+	float = {
+		border = "rounded",
+		source = true,
+	}
+
+})
+
+-- editor setup
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.cmd[[colorscheme elflord]]
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,4 @@
+return  {"nvim-treesitter/nvim-treesitter", branch = 'main', lazy = false, build = ":TSUpdate"}
+
+
+
