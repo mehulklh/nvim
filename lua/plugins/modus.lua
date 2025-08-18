0528449f8448288babc8f1cd3c5d1f9e6989401e
mehulklh <106767917+mehulklh@users.noreply.github.com>
2025-08-18 16:44:22 +0530

    added modus colorscheme

---
+++ lua/plugins/modus.lua
@@ -0,0 +1,11 @@
+return { 
+	"miikanissi/modus-themes.nvim",
+	priority = 1000 ,
+	opts = {
+		styles = {
+			comments = { italic = false },
+			keywords = { italic = false },
+		},
+		style = "modus_vivendi"
+	}
+}
