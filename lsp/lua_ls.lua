26255570d2c9431f34e39d3d6c842ad68ab3417b
mehulklh <106767917+mehulklh@users.noreply.github.com>
2025-08-19 21:11:14 +0530

    added lua completions for neovim Lua API

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -1,11 +1,17 @@
 return {
-	cmd = {"lua-language-server"},
-	filetypes = {"lua"},
+	cmd = { "lua-language-server" },
+	filetypes = { "lua" },
 	settings = {
 		Lua = {
-			hint = {enable = true},
-			telemetry = {enable = false},
-			diagnostics = {globals = {"vim"}},
+			hint = { enable = true },
+			telemetry = { enable = false },
+			diagnostics = { globals = { "vim" } },
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("lua", true), -- enables completion for neovim lua API
+			}
 		}
 	}
 }
+
+
+
