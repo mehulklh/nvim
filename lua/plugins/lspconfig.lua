5426e89e00a77d0105caecc001293231b93a354c
mehulklh <106767917+mehulklh@users.noreply.github.com>
2025-11-18 20:39:55 +0530

    fixed vim is an unknown global warning

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,3 +1,27 @@
 return {
 	"neovim/nvim-lspconfig",
+	config = function()
+		-- lua_ls config to handle vim is an unknown global
+		vim.lsp.config("lua_ls", {
+			settings = {
+				Lua = {
+					runtime = {
+						version = "LuaJIT",
+					},
+					diagnostics = {
+						globals = {
+							"vim",
+							"require",
+						},
+					},
+					workspace = {
+						library = vim.api.nvim_get_runtime_file("", true),
+					},
+					telemetry = {
+						enable = false,
+					},
+				},
+			},
+		})
+	end,
 }
